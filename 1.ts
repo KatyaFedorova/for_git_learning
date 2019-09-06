217225294258ff18ad72a76fb1257de6bd8a905e
KatyaFedorova <legion.is.my.name1984@gmail.com>
2019-09-06 16:46:40 +0300

    test add .node_moduls in .gitignore

--- 1.ts
+++ 1.ts
@@ -1,1 +1,14 @@
-2
+function getAnimals(done) {
+    Animal.get(function (err, animals) {
+        if(err) {
+            return done(err);
+        }
+
+        return done(null, {
+            dogs: animals.dogs,
+            cats: animals.cats
+        })
+    });
+
+    debugger;
+}
